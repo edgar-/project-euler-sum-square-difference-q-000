e31759926e21c34bd6c9d84e2ef105a503115fc3
M. Edgar Joel <mkakajoel@gmail.com>
2016-04-14 00:35:18 +0000

    Solution to P.Euler's sum of squares problem

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,1 +1,18 @@
 # Implement your object-oriented solution here!
+class SumSquareDifference
+  def initialize(num)
+    @num = num
+  end
+
+  def difference
+    square_sum - sum_square
+  end
+
+  def square_sum
+    (1..@num).inject(:+)**2
+  end
+
+  def sum_square
+    (1..@num).inject { |sum, n| sum + n**2 }
+  end
+end
--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,12 @@
 # Implement your procedural solution here!
+  def sum_square_difference(num)
+    square_sum(num) - sum_square(num)
+  end
+
+  def square_sum(num)
+    (1..num).inject(:+)**2
+  end
+
+  def sum_square(num)
+    (1..num).inject { |sum, n| sum + n**2 }
+  end
